a21e41435fd9ad9dbb91c11c7971d42b1c407993
Daniel Castro R <47432198+danielc2203@users.noreply.github.com>
2024-07-09 16:47:41 -0500

    Dash Empleados v1

--- dashboard_employee.php
+++ dashboard_employee.php
@@ -10,15 +10,60 @@ if (!isset($_SESSION['user_id'])) {
 include 'templates/header.php';
 ?>
 
+<!DOCTYPE html>
+<html lang="es">
+    <body class="d-flex flex-column h-100">
+        <main class="flex-shrink-0">
+            <!-- Header-->
+            <header class="py-5">
+                <div class="container px-5 pb-5">
+                    <div class="row gx-5 align-items-center">
+                        <div class="col-xxl-7">
+                            <!-- Header text content -->
+                            <div class="text-center text-xxl-start">
+                                <div ><div class="text-uppercase">Formación &middot; Seguridad &middot; Calidad</div></div>
+                                <div class="fs-3 fw-light text-muted">Te ayudamos a obtener Tu certificación en : </div>
+                                <h2 class="display-3 fw-bolder mb-3"><span class="text-gradient d-inline"></span>manipulación de alimentos</h2>
+                                <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
+                                    <a class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="course_overview.html">Ver Curso</a>
+                                    <a class="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" href="contact.html">Realizar exámen</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xxl-5">
+                            <!-- Header profile picture-->
+                            <div class="d-flex justify-content-center mt-5 mt-xxl-0">
+                                <div class="profile bg-gradient-primary-to-secondary">
+                                    <h1>CERTIFICANDO.COM.CO</h1>                         
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <!-- About Section-->
+            <section class="bg-light py-5">
+                <div class="container px-5">
+                    <div class="row gx-5 justify-content-center">
+                        <div class="col-xxl-8">
+                        <div class="text-center my-5">
+                            <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">Acerca de Nosotros</span></h2>
+                            <p class="lead fw-light mb-4">Somos Certificando.com.co, tu aliado en la formación en manipulación de alimentos.</p>
+                            <p class="text-muted">
+                                En Certificando.com.co, nos especializamos en ofrecer cursos de capacitación en manipulación de alimentos para garantizar que los profesionales del sector alimentario adquieran los conocimientos y habilidades necesarios para cumplir con las normativas sanitarias y asegurar la seguridad alimentaria. 
+                                Nuestro objetivo es proporcionar una formación de calidad que permita a los manipuladores de alimentos desempeñar sus funciones de manera segura y eficiente, protegiendo así la salud de los consumidores.
+                            </p>
+                        </div>
 
-
-<body>
-    <h1>Estudiantes Formulario</h1>
-    <!-- dashboard_company.php -->
-<button id="btnRegistrarUsuario" class="btn btn-primary">Registrar Nuevo Usuario</button>
-
-    <a href="logout.php">Cerrar Sesión</a>
-</body>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+        
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
 </html>
 
 <?php
--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,6 @@
+<?php
+include 'includes/init.php';
+?>
 
 <!DOCTYPE html>
 <html lang="es">
@@ -7,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Capacitación en Alimentos</title>
     <!-- <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -34,7 +37,7 @@
                         </li>
                     <?php else: ?>
                         <li class="nav-item">
-                            <a class="nav-link" href="register.php">Registro</a>
+                            <a class="nav-link"  href="logout.php">Salir</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="login.php">Inicio de Sesión</a>
